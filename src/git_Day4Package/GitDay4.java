c05ab1e8b71fb13588a6ba5e29b8008f5f4d3465
mahmutsalik <62161817+mahmutsalik@users.noreply.github.com>
2020-03-16 20:35:07 -0400

    changed mahmut

--- src/git_Day4Package/GitDay4.java
+++ src/git_Day4Package/GitDay4.java
@@ -8,8 +8,9 @@ public class GitDay4 {
 	
 	
 	public static void main(String[] args) {
-		System.out.println("local firs commit master branch");
+		System.out.println("local first commit master branch");
 		System.out.println("local branch first commit ");
+		System.out.println("Someone else bla ");
 	}
 
 }
